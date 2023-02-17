e2df15d79dadd63547cc63f0a31eade196e36b26
RyandTeague <ryanteague@hotmail.co.uk>
2023-02-17 02:37:22 +0000

    add structure to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import AddTodo from './components/AddToDo';
 import ToDo from './components/ToDo';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <AddTodo />
-      <ToDo />
+    <div className="wrapper">
+      <Container>
+        <Row className='justify-content-center pt-5'>
+          <Col>
+            <Card className='p-5'>
+              <h1>My ToDos</h1>
+              <AddTodo />
+              <ToDo />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
