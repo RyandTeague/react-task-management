4abc159dc5dc3939ac8776861fb146fb51440d19
RyandTeague <ryanteague@hotmail.co.uk>
2023-02-27 02:25:39 +0000

    Set up template code

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
-import styles from './App.module.css';
-import React from 'react';
-import TaskPage from './pages/todos/TaskPage';
-import NavBar from "./components/NavBar";
-import Container from "react-bootstrap/Container";
-import { Routes, Route } from "react-router-dom";
-import "./api/axiosDefaults";
-import SignUpForm from "./pages/auths/SignUpForm";
-import SignInForm from "./pages/auths/SignInForm";
-import { useCurrentUser } from "./contexts/CurrentUserContext";
-import ToDo from './components/ToDo';
-import HomePage from './pages/auths/HomePage'
-import SignUpPage from './pages/auths/SignUpForm';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	const currentUser = useCurrentUser();
-	const profile_id = currentUser?.profile_id || "";
-	class ErrorBoundary extends React.Component {
-		constructor(props) {
-			super(props);
-			this.state = { hasError: false, error: null };
-		}
-
-		static getDerivedStateFromError(error) {
-			// Update state so the next render will show the fallback UI.
-			return { hasError: true, error };
-		}
-
-		render() {
-			if (this.state.hasError) {
-				// You can render any custom fallback UI
-				return <h1>Something went wrong: {this.state.error.message}</h1>;
-			}
-
-			return this.props.children;
-		}
-	}
-
-	return (
-		<div className={styles.App}>
-			<NavBar />
-			<Container className={styles.Main}>
-				<ErrorBoundary>
-					<Routes>
-						<Route path="/" element={<HomePage />} />
-						<Route path="/signin" element={<SignInForm />} />
-						<Route path="/signup" element={<SignUpForm />} />
-						<Route path="/tasks" element={<ToDo />} />
-					</Routes>
-				</ErrorBoundary>
-			</Container>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { CurrentUserProvider } from "./contexts/CurrentUserContext";
-import { ProfileDataProvider } from "./contexts/ProfileDataContext";
-
-// createRoot(document.getElementById("root")).render(
-// <React.StrictMode>
-// <Router>
-// <App />
-// </Router>
-// </React.StrictMode>
-// );
-
-createRoot(document.getElementById("root")).render(
-    <Router>
-        <CurrentUserProvider>
-            <ProfileDataProvider>
-                <App />
-            </ProfileDataProvider>
-        </CurrentUserProvider>
-    </Router>
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
